d613e82f6e6e8dfabbcc64abb795d72dbdad1285
Vikas Sharma <vikaskumarasharma06470@gmail.com>
2021-08-14 20:07:33 +0530

    2nd commit

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import React from 'react';
+import img from "../img/about.jpg";
 
 const Menu = () => {
     return (
         <>
-            <div className="menu">
-                <h1>This Is Menu </h1>
+            <div className="about">
+            <div className="container">
+          <div className="row">
+           
+            <div className="col-6">
+              <div className="about_img">
+                <img src={img} alt="About" />
+              </div>
+            </div>
+            <div className="col-6 p-25">
+              <h3> Menu </h3>
+              <h1> Welcome To MAESTRO PIZZINI </h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
+                vel numquam rerum. Dolorem voluptates quae asperiores,
+                assumenda, eaque eveniet ducimus molestiae et ipsam corporis
+                quis minima sint. Ullam, minima non?
+              </p>
+              <div className="about_btn">
+                <a href="!#" className="btn btn_smart">
+                  READ MORE
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
             </div>
         </>
     )
